399d515b86c37544a6e932cb1511f7967fc27e20
Cynthia Fernandez Diaz <cfernandez diaz@iotc.local>
2024-01-05 11:05:26 +0400

    update January

---
+++ initialization/90_DEFAULTS.R
@@ -0,0 +1,13 @@
+print("Initializing defaults...")
+
+LEGEND_TEXT_SIZE         = 15
+LEGEND_TEXT_SIZE_SMALLER = LEGEND_TEXT_SIZE - 2
+
+GEO_SHOW_HS             = TRUE
+
+GEO_PIEMAP_OPACITY      = NA   #.7 - setting the opacity to a value < 1 increases the rendering time consistently
+GEO_PIEMAP_USE_CENTROID = TRUE
+
+IOTC_SPECIES = c("ALB", "BET", "BLM", "BLT", "BUM", "COM", "FRI", "GUT", "KAW", "LOT", "MLS", "SBF", "SFA", "SKJ", "SWO", "YFT")
+
+print("Defaults initialized!")
---
+++ initialization/92_LIBS_IOTC.R
@@ -0,0 +1,19 @@
+# Install/load pacman
+if(!require(pacman)){
+  install.packages("pacman")
+  suppressPackageStartupMessages(library(pacman,quietly = TRUE))
+}
+
+# Install/load libraries required for analysis
+pacman::p_load("iotc.base.common.plots")
+
+print("Initializing IOTC libs...")
+
+# Initialise species colors
+initialize_all_species_colors()
+initialize_all_gears_colors()
+
+QUALITY_COLORS = all_quality_colors()
+
+print("IOTC libs fully initialized")
+
