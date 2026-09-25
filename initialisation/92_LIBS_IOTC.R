70c1f5da558fc12bd888f813b79ce02c33417342
manuchassot <manu.chassot@gmail.com>
2026-09-25 16:16:51 +0400

    Corrections and addition of index.html to urls

--- initialisation/92_LIBS_IOTC.R
+++ initialisation/92_LIBS_IOTC.R
@@ -3,7 +3,7 @@ print("Loading IOTC R libraries...")
 pacman::p_load("iotc.base.common.plots")
 
 # Initialise species colors
-initialize_all_species_colors()
-initialize_all_gears_colors()
+#initialize_all_species_colors()
+#initialize_all_gears_colors()
 
 print("IOTC R libraries loaded!")
